533e479a98920fdaac1358e2c0f8fc4877066886
Ghina Fadiyah Hr <gfadiyahhr@gmail.com>
2025-09-28 21:03:27 +0700

    testing

--- app/src/main/java/com/muh/faturrahman6706223013/tubesmpro2/MainActivity.kt
+++ app/src/main/java/com/muh/faturrahman6706223013/tubesmpro2/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hi $name!",
+        text = "Hai $name!",
         modifier = modifier
     )
 }
