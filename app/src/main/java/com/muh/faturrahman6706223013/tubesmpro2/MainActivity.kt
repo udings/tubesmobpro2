3f133eae1eb15844f2405af99440767760ea2387
Ghina Fadiyah Hr <gfadiyahhr@gmail.com>
2025-09-28 21:09:26 +0700

    test

--- app/src/main/java/com/muh/faturrahman6706223013/tubesmpro2/MainActivity.kt
+++ app/src/main/java/com/muh/faturrahman6706223013/tubesmpro2/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Tubesmpro2Theme {
-        Greeting("Ghina")
+        Greeting("Ghina Fadiyah Hr")
     }
 }
