e667f13c4087531fc0c8c6e6b06c3fd75c6dd346
Ghina Fadiyah Hr <gfadiyahhr@gmail.com>
2025-09-28 20:54:24 +0700

    First Commit

--- app/src/main/java/com/muh/faturrahman6706223013/tubesmpro2/MainActivity.kt
+++ app/src/main/java/com/muh/faturrahman6706223013/tubesmpro2/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hi $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Tubesmpro2Theme {
-        Greeting("Ghina Fadiyah")
+        Greeting("Ghina")
     }
 }
